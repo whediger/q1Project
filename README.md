6ccd4be931357389eb50879f8430eff59eab6bed
Wes Hediger <wes.hediger@gmail.com>
2016-05-13 22:42:34 -0600

    inital commit

---
+++ README.md
@@ -0,0 +1,15 @@
+
+#Cooking Essentials
+
+##The idea
+I would like to create a web app that is an interface for searching the National Nutritional Database and returns the nutritional information for one or several meals or ingredients. The results may be displayed in graphical format that is as of this time of an undetermined type. (I am hoping to discover something more interesting than a pie chart. Additional features will be determined during production. Possible features may be Ph levels, salt content, other totals of interest to dietary concerns that may be accomplished without the use of persistent information.
+
+##the challange
+The challenge of this project will be to access the most relevant information from a laypersons input to query a database of 30K+ ingredients and 800K+ instances and present it in an easily understandable form to the user.
+
+##The technologies I will be using are:
+	▪	HTML
+	▪	Javascript
+	▪	CSS/SASS
+	▪	JQuery
+
---
+++ js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+var itemKey = "";
+
+  $.get('http://api.nal.usda.gov/ndb/list?format=json&lt=g&sort=n&api_key=rz0uHRvuUkaP6TxlqLvFaVKYKlbUgcjYMOOZE51u ', function(data){
+    var itemLength = data.list.item.length;
+    for ( i = 0; i < itemLength; i++ ) {
+        console.log(data.list.item[i].name);
+        $('#selectCatagory').append('<option'
+          + ' value="' + data.list.item[i].name
+          + '">' + data.list.item[i].name
+          + '</option>')
+    }
+    return data;
+  }).then(function(data){
+    $('#selectCatagory').on('change', function(){
+      console.log($('#selectCatagory').val());
+      $('main').append('<p></p>');
+      $('main p:last-child').text($('#selectCatagory').val());
+    });
+
+    $('#submit').on('click', function(event){
+      event.preventDefault();
+    });//do I really need a whole function for this?
+
+  });
+  // need to get the item id number and then pass it back over to get nutritional information. Two seperate requests.
+});
